ae59334c70f0c7733b941c502562189194213b44
estill01 <ehs.stillman@gmail.com>
2023-03-16 03:12:46 -0700

    Refactor: Integrate BudgetTracker into TwitterBot so it knows if it has enough budget to tweet

--- gptwitter_bot/twitter_bot.py
+++ gptwitter_bot/twitter_bot.py
@@ -1,63 +1,79 @@
 import tweepy
-from gptwitter_bot.trackers import BudgetTracker
-from gptwitter_bot.trackers import StatTracker
-from gptwitter_bot.cost_calculation import CostCalculator
+from gptwitter_bot.trackers import BudgetTracker, StatTracker
+from gptwitter_bot.cost_calculator import CostCalculator
 
 
 class TwitterBot:
-     def __init__(self, budget, pricing, handle, models):
-         self.budget_tracker = BudgetTracker(budget, pricing)
-         self.stat_tracker = StatTracker() # TODO .. actually use this, or delete
-         self.handle = handle
-         self.models = models
+    def __init__(self, api_key, api_key_secret, access_token, access_token_secret, budget_tracker, handle, models):
+        self.auth = tweepy.OAuthHandler(api_key, api_key_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+        self.twitter_api = tweepy.API(self.auth)
+        
+        self.budget_tracker = budget_tracker
+        self.stat_tracker = StatTracker()
+        self.handle = handle
+        self.models = models
+        self.cost_calculator = CostCalculator()
 
     def generate_response(self, prompt, model_key):
-        tokens = self.budget_tracker.calculate_tokens(prompt, 150)
-        if self.budget_tracker.update_budget(tokens):
-            with self.rate_limiter:
-                response = openai.Completion.create(
-                    engine=self.models[model_key],
-                    prompt=prompt,
-                    temperature=0.7,
-                    max_tokens=150,
-                    n=1,
-                    stop=None,
-                    top_p=1,
-                )
+        tokens = self.cost_calculator.count_tokens(prompt)
+        cost = self.cost_calculator.calculate_cost(tokens)
+        if self.budget_tracker.can_spend(cost):
+            self.budget_tracker.spend(cost)
+            response = openai.Completion.create(
+                engine=self.models[model_key],
+                prompt=prompt,
+                temperature=0.7,
+                max_tokens=150,
+                n=1,
+                stop=None,
+                top_p=1,
+            )
             return response.choices[0].text.strip()
+
+    def send_tweet(self, tweet_text):
+        tokens = self.cost_calculator.count_tokens(tweet_text)
+        cost = self.cost_calculator.calculate_cost(tokens)
+
+        if self.budget_tracker.can_spend(cost):
+            self.budget_tracker.spend(cost)
+            self.twitter_api.update_status(status=tweet_text)
+            print(f"Tweet sent: {tweet_text}")
+        else:
+            print("Insufficient budget to send the tweet")
+
+    def start_stream(self, keywords):
+        listener = TwitterBotStreamListener(api=self.twitter_api, twitter_bot=self)
+        stream = tweepy.Stream(auth=self.auth, listener=listener)
+        stream.filter(track=keywords, is_async=True)
 
 
 class TwitterBotStreamListener(tweepy.StreamListener):
-     def __init__(self, api, twitter_bot):
-         super().__init__(api)
-         self.twitter_bot = twitter_bot
+    def __init__(self, api, twitter_bot):
+        super().__init__(api)
+        self.twitter_bot = twitter_bot
 
-     def on_status(self, status):
-         # TODO Fix; currently ignores retweets AND replies
-         if hasattr(status, "retweeted_status") or status.in_reply_to_status_id:
-             return
+    def on_status(self, status):
+        if hasattr(status, "retweeted_status") or status.in_reply_to_status_id:
+            return
 
-         # Check if you're allowed to replenish budget (i.e. it's been >= 30 days)
-         self.twitter_bot.budget_tracker.replenish_budget()
+        self.twitter_bot.budget_tracker.reset_budget_cycle()
 
-         user_question = status.text
-         prompt = f"User asked: {user_question}\n{self.twitter_bot.handle} response:"
-         # TODO Fix -- choose model more dynamically
-         model_key = "gpt_4_8k"  
-         tokens = self.twitter_bot.budget_tracker.calculate_tokens(prompt, 150)
+        user_question = status.text
+        prompt = f"User asked: {user_question}\n{self.twitter_bot.handle} response:"
+        model_key = "gpt_4_8k"
+        tokens = self.twitter_bot.cost_calculator.count_tokens(prompt)
+        cost = self.twitter_bot.cost_calculator.calculate_cost(tokens)
 
-         if self.twitter_bot.budget_tracker.update_budget(tokens):
-             response = self.twitter_bot.generate_response(prompt, model_key)
+        if self.twitter_bot.budget_tracker.can_spend(cost):
+            response = self.twitter_bot.generate_response(prompt, model_key)
 
-         if response:
-             self.twitter_bot.budget_tracker.spend_tracker.log_tweet(status.id)
+            if response:
+                self.twitter_bot.budget_tracker.spend_tracker.log_tweet(status.id)
+                reply = f"@{status.user.screen_name} {response}"
+                self.api.update_status(status=reply, in_reply_to_status_id=status.id)
 
-             reply = f"@{status.user.screen_name} {response}"
-             self.api.update_status(status=reply, in_reply_to_status_id=status.id)
-
-     def on_error(self, status_code):
-         if status_code == 420:
-             return False
-
-
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
 
